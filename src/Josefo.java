2c1eaeba22b7e5cf9be5ef2fa8af38959f492ba2
1Maty <108903917+1Maty@users.noreply.github.com>
2023-04-06 23:56:24 -0300

    Update Josefo.java

--- src/Josefo.java
+++ src/Josefo.java
@@ -17,23 +17,26 @@ public class Josefo {
         Node coso= colIntegrantes.head;
         while (colIntegrantes.size()!=1){
             if(posicion%m==0){
-                lista2.add(colIntegrantes.get(colIntegrantes.posicion(coso.data)));
+                lista2.add(coso.data);
                 colIntegrantes.remove(colIntegrantes.posicion(coso.data));
-               }
+            }
             coso=coso.next;
             posicion++;
-           }
+        }
         return lista2;
     }
     public  static void main(String[] args){
         CircularLinkedList listuti = new CircularLinkedList();
-        listuti.add(10);
-        listuti.add(20);
-        listuti.add(30);
-        listuti.add(40);
-        listuti.add(50);
-        listuti.add(60);
-        listuti.add(70);
-        juego(2,listuti).mostrar();
+        listuti.add(1);
+        listuti.add(2);
+        listuti.add(3);
+        listuti.add(4);
+        listuti.add(5);
+        listuti.add(6);
+        listuti.add(7);
+        listuti.add(8);
+        listuti.add(9);
+
+        juego(4,listuti).mostrar();
     }
 }
